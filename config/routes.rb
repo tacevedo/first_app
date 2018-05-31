3f7f13135dfc1e4770fcca7e5caa28970af04d23
tam <tamara.acevedoavila@gmail.com>
2018-05-30 20:05:19 -0400

    commit 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/one'
+  get 'pages/two'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#one'
 end
